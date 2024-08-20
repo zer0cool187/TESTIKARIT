724d891373790823640a47af835c634a162d122a
zer0cool187 <narcosks1@gmail.com>
2024-08-20 13:06:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def hello():
-    print("hi")
-  
-def bye():
-    print("bye")
-      
-print(hello())
+>>> pd.get_dummies(pd.Series(list('abcaa')), drop_first=True)
+       b      c
+0  False  False
+1   True  False
+2  False   True
+3  False  False
+4  False  False
